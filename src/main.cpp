81716e93eb4a5861e489c8d9792f8a5fade0c35d
Jaraslau Zaitsau <122669697+jaraslauzaitsau@users.noreply.github.com>
2026-05-26 11:34:05 +0200

    Fix poor font size detection and UI bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <raylib.h>
+#include <raygui.h>
 #include "ui/UI.hpp"
 #include "ui/pages/MainPage.hpp"
 
-UI ui(new MainPage());
-
 int main(int argc, char* argv[]) {
     InitWindow(800, 600, "Battleship");
+    Font font = GetFontDefault();
+    GuiSetFont(font);
+    GuiSetStyle(DEFAULT, TEXT_SIZE, 24);
     SetTargetFPS(60);
+
+    UI ui(new MainPage());
 
     while (true) {
         if (WindowShouldClose()) {
